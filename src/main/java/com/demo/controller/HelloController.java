22358b28c603b46d01430be704f879b850d8e600
Rain <shucsrain@163.com>
2025-04-18 13:18:26 +0800

    提交nacos相关

--- src/main/java/com/demo/controller/HelloController.java
+++ src/main/java/com/demo/controller/HelloController.java
@@ -1,13 +1,27 @@
 package com.demo.controller;
 
+//import com.alibaba.nacos.api.config.annotation.NacosValue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
+
+//    @Value("${sample.txt}")
+    private String sample;
 
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @RequestMapping("/get")
+    public String get(){
+        log.info("get request,return {}",sample);
+
+        return sample;
     }
 }
--- src/main/java/com/demo/xxy/DemoApplication.java
+++ src/main/java/com/demo/xxy/DemoApplication.java
@@ -3,6 +3,7 @@ package com.demo.xxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 //@EnableSwagger2
 @ComponentScan(value = "com.demo.*")
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
